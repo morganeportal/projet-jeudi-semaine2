66b36d83f9ddec9dd2d6df5a8ed76dd7a306eddb
morganeportal <morganeportal@hotmail.fr>
2018-07-12 12:08:24 +0200

    modif mairies

--- mairie.rb
+++ mairie.rb
@@ -7,18 +7,16 @@ def get_the_email_of_a_townhal_from_its_webpage(url)
 	page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
-#puts(get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/vaureal.html"))
-
 def get_all_the_urls_of_val_doise_townhalls()
 	page = 	Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))  
 	a = page.xpath('//p/a')
 	result = {}
 	a.each do  |node| 
-		result[node.text] ='http://annuaire-des-mairies.com'+node['href']
+		url = 'http://annuaire-des-mairies.com'+node['href']
+		email = get_the_email_of_a_townhal_from_its_webpage(url)
+		result[node.text]= email
 	end
 	return result
 end
 
-
 puts(get_all_the_urls_of_val_doise_townhalls)
-
