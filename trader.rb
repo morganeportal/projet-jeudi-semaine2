eeb59f23b32ae5747171a29e51160bf25305ff11
morganeportal <morganeportal@hotmail.fr>
2018-07-12 14:15:22 +0200

    modif trader

--- trader.rb
+++ trader.rb
@@ -1,15 +1,24 @@
 require 'rubygems'
 require 'nokogiri'   
 require 'open-uri'
+require 'pry'
 
 def get_the_moneyhash()
-	result = {}
+	result = []
 	page = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))  
 	a = page.xpath('//tbody/tr')
-	#a = page.xpath('//*[@id="id-bitcoin"]')
+
 	for node in a
 		data = node.text.split
-		result[data[2]] = data[5]
+		#binding.pry
+		nom = data[2]
+		prix = data[5]
+		devise = {
+			'name' => nom ,
+			'price' => prix ,
+		}
+		#binding.pry
+		result.push devise
 	end
 	return result
 end
